f3ee787a11edc42a4f89cd7f3617470691ddf4a4
halloweenkid9 <mrfudge18@gmail.com>
2019-04-04 13:19:31 -0700

    Add files via upload

---
+++ TestController1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController1 : MonoBehaviour {
+
+    Rigidbody rigb;
+    SphereCollider col;
+    Component TriggerSphere;
+	// Use this for initialization
+	void Start () {
+        TriggerSphere = GameObject.Find("TriggerSphere").GetComponent("TestAttachment1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && TestAttachment1.InRange)
+        {
+            rigb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        }
+	}
+}
